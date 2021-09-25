e1d707ac230d8b9205b429ce0a01f2420cf9260d
kam-ada <ada.kam208@gmail.com>
2021-09-25 15:56:39 +0800

    version1

--- codeitsuisse/routes/arena.py
+++ codeitsuisse/routes/arena.py
@@ -8,11 +8,10 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 @app.route('/tic-tac-toe', methods=['POST'])
-def evaluate123():
+def evaluateBattleId():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input")
-    result = inputValue * inputValue
+    battleId = data.get("battleId")
     logging.info("My result :{}".format(result))
     return json.dumps(result)
 
---
+++ codeitsuisse/routes/asteroids.py
@@ -0,0 +1,90 @@
+import logging
+import json
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/asteroid', methods=['POST'])
+
+
+def evaluateAsteroid():
+    data = request.get_json('[...]')
+    logging.info("data sent for evaluation {}".format(data))
+    input = data.get("test_cases")
+    result = []
+    for line in input:
+        score = algorithm(line) 
+        temp = {}
+        temp.update({"input": line})
+        temp.update({"score": score[0]})
+        temp.update({"origin": score[1]})
+        result.append(temp)
+    logging.info("My result :{}".format(result))
+    return json.dumps(result)
+
+def algorithm (text):
+    origin = 0
+    final_origin = 0
+    final_score = 0
+    sub_ttl = 1
+    total = 0
+    for position in text:
+        left = 1
+        right = 1
+        while (origin - left >= 0 or origin + right < len(text)):
+            left_prev = text[origin - left + 1]
+            right_prev = text[origin + right - 1]
+            if (origin - left < 0):
+                right_cur = text[origin + right]
+                if (right_cur == right_prev):
+                    sub_ttl += 1
+                    right += 1
+                else:
+                    break
+            elif (origin + right >= len(text)):
+                left_cur = text[origin - left]
+                if (left_cur == left_prev):
+                    sub_ttl += 1
+                    left += 1
+                else:
+                    break
+            else:
+                left_cur = text[origin - left]
+                right_cur = text[origin + right]
+                if (left_cur == right_cur):
+                    if (left_cur != left_prev):
+                        total += calc (sub_ttl)
+                        sub_ttl = 0
+                    sub_ttl += 2
+                    left += 1
+                    right += 1
+                else:
+                    if (left_cur == left_prev):
+                        sub_ttl += 1
+                        left += 1
+                    elif (right_cur == right_prev):
+                        sub_ttl += 1
+                        right += 1
+                    else:
+                        break
+        total += calc(sub_ttl)
+        sub_ttl = 1
+        print("Origin: " + str(origin) + " Score: " + str(total))
+        if (total > final_score):
+            final_score = total
+            final_origin = origin
+        origin += 1
+        total = 0
+
+    return final_score, origin
+
+def calc (num):
+    if num >= 10:
+        return num * 2
+    elif num >= 7:
+        return num * 1.5
+    else:
+        return num
